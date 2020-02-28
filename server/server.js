676d8748b5b901895d9265cbfd8f1112929565da
Ubuntu <ubuntu@ip-172-31-50-46.us-west-2.compute.internal>
2020-02-28 23:56:00 +0000

    Update for deployment settings

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
-// require('newrelic');
+require('newrelic');
 const express = require('express');
 const axios = require('axios');
 
 const app = express();
-
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
 const dirPath = `${__dirname}/../public/dist/`;
 app.use(express.static(dirPath));
 
 const { routes } = require('../proxyconfig.json');
+
+app.get('/api/:room_id', (req, res) => {
+	const { room_id } = req.params;
+	Promise.all([
+		axios.get(`${routes['listings'].address}/${room_id}`).then(data => data.data).catch(e => e),
+		axios.get(`${routes['morehomes'].address}/${room_id}`).then(data => data.data).catch(e => e),
+		axios.get(`${routes['reviews'].address}/${room_id}`).then(data => data.data).catch(e => e),
+	])
+	.then( (...args) => {
+		const data = {};
+		data['listings'] = args[0];
+		data['morehomes'] = args[1];
+		data['reviews'] = args[2];
+		res.status(200).send(data);
+	})
+	.catch(e => {
+		console.log(e);
+		res.status(500).send('Error');
+	});
+});
 
 app.get('/api/:component/:room_id', (req, res) => {
   const { component, room_id } = req.params;
